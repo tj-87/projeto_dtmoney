590980fc872a4bfad95b2f5d64ce1296312ba3ab
Thiago Silva <thiago380tt@gmail.com>
2022-07-01 22:54:34 -0300

    modificações styles global e NewTransactionModal

--- src/components/NewTansactionModal/index.tsx
+++ src/components/NewTansactionModal/index.tsx
@@ -1,19 +1,31 @@
-import Modal from 'react-modal';
+import Modal from "react-modal";
+import { Container } from "./style";
 
 interface NewTransactionModalProps {
-    isOpen: boolean;
-    onRequestClose: ()=> void;
+  isOpen: boolean;
+  onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
-    return (
-
-      <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  return (
+    <Modal 
+    isOpen={isOpen} 
+    onRequestClose={onRequestClose}
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
     >
-      <h2>Cadastrar transações</h2>
-    </Modal>
+      <Container>
+        <h2>Cadastrar transações</h2>
 
-    );
+        <input placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
 }
